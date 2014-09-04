a4a17501e1d3cfdcf69cca382098d215f9454156
yangyi <yangyi.cn.gz@gmail.com>
2014-09-04 12:28:44 +0800

    重构service manager

---
+++ test/service/manager.js
@@ -0,0 +1,68 @@
+"use strict";
+
+var manager = require(__dirname+'/../../lib/service/manager');
+var assert = require('assert');
+
+describe('Service manager', function() {
+    var TestService = function(config) {
+        this.config = config;
+    }
+
+    manager.define({
+        foo: {
+            generator: function(config) {
+                return new TestService(config);
+            },
+            name: 'foo',
+        },
+        bar: {
+            __EXTEND__: 'foo',
+            name: 'bar',
+        },
+        foobar: function(id) {
+            return (id % 2) ? 'foo' : 'bar';
+        }
+    });
+
+    it('should throw error when get undefined service', function() {
+        assert.throws(function() {
+            manager.get('baz');
+        }, /undefined/i);
+    });
+
+    it('should throw error when service generator is undefined', function() {
+        assert.throws(function() {
+            manager.define('baz', {name: 'baz'});
+            manager.get('baz');
+        }, /generator/);
+    });
+
+    it('should throw error when service dispatcher not return service name', function() {
+        assert.throws(function() {
+            manager.define('baz', function(id) {});
+            manager.get('baz', 1);
+        }, /dispatcher/);
+    });
+
+    it('should return service instance by name', function() {
+        var foo = manager.get('foo');
+        assert.ok(foo instanceof TestService);
+        assert.equal(foo.config.name, 'foo');
+    });
+
+    it('should extends config by "__EXTEND__"', function() {
+        var bar = manager.get('bar');
+        assert.ok(bar instanceof TestService);
+        assert.equal(bar.config.name, 'bar');
+    });
+
+    it('should return service instance by dispatcher function', function() {
+        var foo = manager.get('foobar', 1);
+        assert.ok(foo instanceof TestService);
+        assert.equal(foo.config.name, 'foo');
+
+        var bar = manager.get('foobar', 2);
+        assert.ok(bar instanceof TestService);
+        assert.equal(bar.config.name, 'bar');
+    });
+});

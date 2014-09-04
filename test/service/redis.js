10c9b52efa1332f7f8c577cfe028220367f9e6a8
yangyi <yangyi.cn.gz@gmail.com>
2014-09-05 00:01:37 +0800

    重构redis adapter

---
+++ test/service/redis.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var Promise = require('bluebird');
+var anyorm = require(__dirname+'/../../lib');
+var Service = anyorm.Service;
+var assert = require('assert');
+
+describe('Redis', function() {
+    Service.define({
+        'redis': {
+            generator: function(options) {
+                return Service.Redis.createClient(options);
+            },
+            host: '127.0.0.1',
+            port: 6379,
+        },
+        'redis_pool': {
+            generator: function(options) {
+                return Service.Redis.createPool(options.pool, options.client);
+            },
+            pool: {
+                max: 5,
+                min: 2,
+            },
+            client: {
+                host: '127.0.0.1',
+                port: 6379,
+            }
+        }
+    });
+
+    it('client', function(done) {
+        this.timeout(1000);
+
+        var redis = Service.get('redis');
+
+        redis.ping(function(error, result) {
+            assert.equal(result, 'PONG');
+        });
+
+        redis.quit(done);
+    });
+
+    it('pool', function(done) {
+        this.timeout(2000);
+
+        var redis = Service.get('redis_pool');
+
+        redis.execute('ping')
+            .then(function(result) {
+                assert.equal(result, 'PONG')
+            })
+            .finally(function() {
+                redis.disconnect().then(function() { done(); });
+            });
+    });
+});
+

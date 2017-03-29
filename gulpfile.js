b7cb9b54a35c57b375a70cc71e46eb584f6fd8de
yangyi <yangyi.cn.gz@gmail.com>
2017-03-29 18:43:13 +0800

    增加ava测试命令

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
-let gulp = require('gulp');
-let ts = require('gulp-typescript');
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const ava = require("gulp-ava");
+const _ = require("lodash");
 
-gulp.task('watch', () => {
-    gulp.watch(['src/**/*.ts', 'tests/**/*.ts'], ['compile']);
+gulp.task("watch", () => {
+    gulp.watch(["src/**/*.ts", "tests/**/*.ts"], ["compile"]);
 });
 
-gulp.task('compile', () => {
-    let tsProject = ts.createProject('tsconfig.json');
+gulp.task("watch:test", () => {
+    gulp.watch("./built/tests/**/*.js", _.debounce(() => {
+        gulp.start("test");
+    }), 3000);
+});
+
+gulp.task("test", () => {
+    gulp.src("./built/tests").pipe(ava({ verbose: true }));
+});
+
+gulp.task("compile", () => {
+    let tsProject = ts.createProject("tsconfig.json");
 
     return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest('built'));
+        .js.pipe(gulp.dest("built"));
 });
 
-gulp.task('default', ['compile']);
+gulp.task("default", ["compile"]);

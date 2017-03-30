cb619bab45133b391a1240dbc288848fad259b70
yangyi <yangyi.cn.gz@gmail.com>
2017-03-30 17:09:34 +0800

    修改ava运行配置

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,9 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
-const ava = require("gulp-ava");
 const _ = require("lodash");
 
 gulp.task("watch", () => {
     gulp.watch(["src/**/*.ts", "tests/**/*.ts"], ["compile"]);
-});
-
-gulp.task("watch:test", () => {
-    gulp.watch("./built/tests/**/*.js", _.debounce(() => {
-        gulp.start("test");
-    }), 3000);
-});
-
-gulp.task("test", () => {
-    gulp.src("./built/tests").pipe(ava({ verbose: true }));
 });
 
 gulp.task("compile", () => {
